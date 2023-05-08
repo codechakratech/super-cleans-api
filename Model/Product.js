24231a70843bab25fbcc691dd4a0093173337d47
codechakratech <81754948+codechakratech@users.noreply.github.com>
2023-05-08 17:32:42 +0530

    Update Product.js

--- Model/Product.js
+++ Model/Product.js
@@ -5,6 +5,10 @@ const productSchema = new mongoose.Schema({
         type:String,
         required:true
     },
+    perKgAmount:{
+    type:Number,
+    required:true   
+    },
     createdby:{
         type:String
     },
